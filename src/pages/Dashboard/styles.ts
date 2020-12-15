d1c1f559631aa41d554e0a47172538c93010b0ce
Alison Bessa <alison.bessa@webjump.com.br>
2020-12-15 08:39:28 -0300

    handle errors

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,5 +1,9 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { shade } from 'polished';
+
+interface FormProps {
+    hasError: boolean
+}
 
 export const Title = styled.h1`
     font-size: 48px;
@@ -10,7 +14,7 @@ export const Title = styled.h1`
     margin-top: 80px;
 `;
 
-export const Form = styled.form`
+export const Form = styled.form<FormProps>`
     margin-top: 40px;
     max-width: 700px;
 
@@ -23,9 +27,15 @@ export const Form = styled.form`
         border: 0;
         border-radius: 5px 0 0 5px;
         color: #3A3A3A;
+        border: 2px solid #FFF;
+        border-right: 0;
+
+        ${(props) => props.hasError && css`
+            border-color: #c53030;
+        `}
 
         &::placeholder {
-            color: #A8A8B3
+            color: #A8A8B3;
         }
     }
 
@@ -95,4 +105,10 @@ export const Repositories = styled.div`
             color: #cbcbd6
         }
     }
+`;
+
+export const Error = styled.span`
+    display: block;
+    color: #c53030;
+    margin-top: 8px;
 `;
